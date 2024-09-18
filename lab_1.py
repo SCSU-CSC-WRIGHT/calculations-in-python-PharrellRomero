a83a067965fa3daa1bf21025767f892030ac280c
PharrellRomero <148895204+PharrellRomero@users.noreply.github.com>
2024-09-18 16:23:36 -0400

    All 3 labs

---
+++ lab_1.py
@@ -0,0 +1,21 @@
+#Lab1
+P=int(input("Enter the principle amount: "))
+R=int(input("Enter the rate of interest: "))
+T=int(input("Enter the time period(in years): "))
+simp=P*R*T/100
+print(simp)
+
+print("-"*100)
+
+#Lab2
+kg=int(input("Enter your weight: "))
+m=float(input("Enter your height: "))
+bmi=kg/m**2
+print(bmi)
+
+print("-"*100)
+
+#Lab3
+c=int(input("Enter the temperature in Celsius: "))
+m=float(c*9/5) + 32
+print(m)
